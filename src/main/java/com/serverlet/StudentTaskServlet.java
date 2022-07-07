e2498a783b08e1aa0aec43e67b614b5360c47bae
Resitas <2228518996@qq.com>
2022-07-07 15:57:29 +0800

    加上了zxq的页面

--- src/main/java/com/serverlet/StudentTaskServlet.java
+++ src/main/java/com/serverlet/StudentTaskServlet.java
@@ -1,5 +1,82 @@
 package com.serverlet;
 
-public class StudentTaskServlet {
+import com.mapper.submissionMapper;
+import com.mapper.taskMapper;
+import com.test.pojo.submission;
+import com.test.pojo.task;
+import jdk.internal.util.xml.XMLStreamException;
+import jdk.internal.util.xml.impl.XMLWriter;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet("/task")
+public class StudentTaskServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+
+        /*访问数据库，生成一个HTML文件*/
+        String resource = "mybatis-config.xml";
+        InputStream is = Resources.getResourceAsStream(resource);
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
+        //获取SqlSession对象，来执行sql
+        SqlSession sqs = factory.openSession();
+        //执行sql
+        taskMapper taskMapper = sqs.getMapper(taskMapper.class);
+        submissionMapper submissionMapper = sqs.getMapper(submissionMapper.class);
+
+        List<task> tasks = taskMapper.selectByKey(id);
+
+        task t = tasks.get(0);
+
+        PrintWriter printWriter = resp.getWriter();
+        StringBuilder response = new StringBuilder();
+
+        response.append("<root>\n" +
+                "<name>项目A101：坤坤的偶像剧评鉴</name>\n" +
+                "<startTime>2022-07-07</startTime>\n" +
+                "<endTime>2022-08-07</endTime>\n" +
+                "<description>\n" +
+                t.getDescription() +
+                "</description>\n");
+        response.append("<submissions num=\"" + t.getSubmitNum() + "\">");
+        int num = Integer.parseInt(t.getSubmitNum());
+        List<submission> submissions = submissionMapper.selectByKey(t.getFirstsm());
+//        while(true)
+//        {
+        response.append("<submission>");
+        response.append("<submissionName>" + "submissions.get(0).getName()" + "</submissionName>");
+        response.append("<data-father>\n" +
+                "守护全世界最好的坤坤️守护全世界最好的坤坤️\n" +
+                "</data-father>");
+        response.append("<data-button>\n" +
+                "                <button class=\"button-submitted\">已提交</button>\n" +
+                "                <button class=\"button-submitted\">查看附件</button>\n" +
+                "            </data-button>");
+        response.append("</submission>");
+//            if(submissions.get(0).getNext()==null)
+//                break;
+//        }
+        sqs.commit();
+        sqs.close();
+        printWriter.write(String.valueOf(response));
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 }
--- src/main/java/com/test/pojo/submission.java
+++ src/main/java/com/test/pojo/submission.java
@@ -8,5 +8,95 @@ public class submission {
     String next;
     String startTime;
     String deadLine;
+
+    @Override
+    public String toString() {
+        return "submission{" +
+                "submitID='" + submitID + '\'' +
+                ", name='" + name + '\'' +
+                ", submitStatus='" + submitStatus + '\'' +
+                ", judgeStatus='" + judgeStatus + '\'' +
+                ", next='" + next + '\'' +
+                ", startTime='" + startTime + '\'' +
+                ", deadLine='" + deadLine + '\'' +
+                ", submitTeams='" + submitTeams + '\'' +
+                '}';
+    }
+
+    public submission(String submitID, String name, String submitStatus, String judgeStatus, String next, String startTime, String deadLine, String submitTeams) {
+        this.submitID = submitID;
+        this.name = name;
+        this.submitStatus = submitStatus;
+        this.judgeStatus = judgeStatus;
+        this.next = next;
+        this.startTime = startTime;
+        this.deadLine = deadLine;
+        this.submitTeams = submitTeams;
+    }
+
+    public String getSubmitID() {
+        return submitID;
+    }
+
+    public void setSubmitID(String submitID) {
+        this.submitID = submitID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubmitStatus() {
+        return submitStatus;
+    }
+
+    public void setSubmitStatus(String submitStatus) {
+        this.submitStatus = submitStatus;
+    }
+
+    public String getJudgeStatus() {
+        return judgeStatus;
+    }
+
+    public void setJudgeStatus(String judgeStatus) {
+        this.judgeStatus = judgeStatus;
+    }
+
+    public String getNext() {
+        return next;
+    }
+
+    public void setNext(String next) {
+        this.next = next;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getDeadLine() {
+        return deadLine;
+    }
+
+    public void setDeadLine(String deadLine) {
+        this.deadLine = deadLine;
+    }
+
+    public String getSubmitTeams() {
+        return submitTeams;
+    }
+
+    public void setSubmitTeams(String submitTeams) {
+        this.submitTeams = submitTeams;
+    }
+
     String submitTeams;
 }
