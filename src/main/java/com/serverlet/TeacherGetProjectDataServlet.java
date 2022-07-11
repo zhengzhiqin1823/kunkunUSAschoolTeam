552080282dabcc7ec7b544dd07e5171f9e092a73
Elaina Kaslana <2964491850@qq.com>
2022-07-11 21:01:06 +0800

    update getJudgeData

--- src/main/java/com/serverlet/TeacherGetProjectDataServlet.java
+++ src/main/java/com/serverlet/TeacherGetProjectDataServlet.java
@@ -6,12 +6,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/teacherGetProjectData")
 public class TeacherGetProjectDataServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String need=req.getParameter("need").toString();
+        if(need.equals("rid")){
+            System.out.println("ok");
+            resp.setContentType("text/text;charset=utf-8");
+            resp.setCharacterEncoding("utf-8");
+            resp.setStatus(200);
+            PrintWriter printWriter = resp.getWriter();
 
+            //getRids
+
+            printWriter.write("rids");
+        }
     }
 
     @Override
