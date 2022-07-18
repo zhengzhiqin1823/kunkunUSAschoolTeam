0c52ed62513aa4e760c4d70336dc6e5e0ceb7da3
ryukiooo <108216145+ryukiooo@users.noreply.github.com>
2022-07-18 17:42:13 +0800

    管理员登录识别

--- src/main/java/com/serverlet/SubmissionReportResultServlet.java
+++ src/main/java/com/serverlet/SubmissionReportResultServlet.java
@@ -18,6 +18,30 @@ import java.util.List;
 public class SubmissionReportResultServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        boolean isLogin = false;
+        HttpSession session = request.getSession();
+        Cookie[] cookies = request.getCookies();
+        System.out.println("id:"+session.getAttribute("id"));
+        System.out.println("type:"+session.getAttribute("type"));
+        Object id = session.getAttribute("id");
+        Object type = session.getAttribute("type");
+
+        if(id!=null&&type!=null) {
+            if(type.equals("admin"))
+                for (Cookie cookie : cookies) {
+                    if (cookie.getName().equals("id")) {
+                        if (cookie.getValue().equals(id)) {
+                            isLogin = true;
+                            break;
+                        }
+                    }
+                }
+        }
+        //如果没有登陆,则返回登陆
+        if (!isLogin) {
+            response.sendRedirect("/0628JavaWebExercise_war");
+            return;
+        }
         String submitID = request.getParameter("submitID");
         String taskid=request.getParameter("taskid");
         request.setAttribute("taskid",taskid);
